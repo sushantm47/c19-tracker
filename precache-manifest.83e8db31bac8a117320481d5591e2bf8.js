66ab16074f7a8f80146faa81660ff2d216ffdaaf
Sushant <vettel@pop-os.localdomain>
2020-04-27 15:47:11 +0530

    Updates

---
+++ precache-manifest.83e8db31bac8a117320481d5591e2bf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202a9394f27bbe357a6a11765d8c5a42",
+    "url": "/c19-tracker-app/index.html"
+  },
+  {
+    "revision": "5708d62727d0f64bf52f",
+    "url": "/c19-tracker-app/static/css/main.810becba.chunk.css"
+  },
+  {
+    "revision": "84134263a599ed8d0dc0",
+    "url": "/c19-tracker-app/static/js/2.ea900baa.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/c19-tracker-app/static/js/2.ea900baa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5708d62727d0f64bf52f",
+    "url": "/c19-tracker-app/static/js/main.cc45ddcc.chunk.js"
+  },
+  {
+    "revision": "0fd11db9bbeea31216bb",
+    "url": "/c19-tracker-app/static/js/runtime-main.c32671fd.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/c19-tracker-app/static/media/image.d7265326.png"
+  }
+]);
